a4c516fe982615642c90be86911f21728c08cbbe
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-07-27 09:29:49 +0200

    Finish day 6

---
+++ 2016/06/part2.cpp
@@ -0,0 +1,31 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <climits>
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+	std::ifstream input{ "input" };
+	std::string line{};
+	std::vector<std::map<char, int>> freq(8);
+	while (std::getline(input, line))
+		for (size_t i{}; i != line.size(); i++)
+			freq[i][line[i]]++;
+
+	std::string msg{};
+
+	for (const std::map<char, int>& f : freq)
+	{
+		std::pair<char, int> maxFreq{ 0, INT_MAX };
+		for (const auto& pair : f)
+			if (pair.second < maxFreq.second)
+				maxFreq = pair;
+		msg.push_back(maxFreq.first);
+	}
+
+	std::cout << msg << std::endl;
+
+	return 0;
+}
---
+++ template/part1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+	std::ifstream input{ "input" };
+	std::string line{};
+	while (std::getline(input, line))
+	{
+	}
+
+	return 0;
+}
